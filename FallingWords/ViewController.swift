3bcad5859029742622f28a4fb86de149404395e4
vnavassa <vnavassa@its.jnj.com>
2018-10-09 18:19:41 +0100

    created basic UI with falling word

--- FallingWords/ViewController.swift
+++ FallingWords/ViewController.swift
@@ -1,20 +1,51 @@
-//
-//  ViewController.swift
-//  FallingWords
-//
-//  Created by Victor on 09/10/2018.
-//  Copyright © 2018 Victor. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var wordToTranslateLabel: UILabel!
+    @IBOutlet weak var newWordLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    
+    var isWordMoving = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.layoutIfNeeded()
+        moveWordDown()
     }
 
+    func moveWordDown() {
+        if isWordMoving { return }
+        
+        UIView.animate(withDuration: 3.0, delay: 0.0, options: [.curveEaseInOut , .allowUserInteraction], animations: {
+            self.isWordMoving = true
+            self.newWordLabel.center = CGPoint(x: self.containerView.frame.midX, y: self.containerView.frame.maxY + 8)
+        }, completion: { finished in
+            
+            self.resetInitialPosition()
+        })
+    }
+    
+    func resetInitialPosition() {
+        if isWordMoving { return }
+        
+        UIView.animate(withDuration: 0.0, delay: 0.0, options: [.curveEaseInOut , .allowUserInteraction], animations: {
+            self.newWordLabel.center = CGPoint(x: self.containerView.frame.midX, y: self.containerView.frame.minY)
+        }, completion: { finished in
+//            self.moveWordDown()
+        })
+    }
 
+    @IBAction func okPressed(_ sender: Any) {
+        if !isWordMoving { return }
+    }
+    
+    @IBAction func notOkPressed(_ sender: Any) {
+        if !isWordMoving { return }
+    }
 }
 
